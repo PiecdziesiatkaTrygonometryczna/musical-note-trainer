fd6dd402914302aa77729cc247967e5c3c0cc7d0
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-02 15:29:13 +0200

    beta of improved displaying

--- scripts/toggle-settings.js
+++ scripts/toggle-settings.js
@@ -4,5 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     toggleSettingsButton.addEventListener('click', function () {
         settingsPanel.classList.toggle('show-settings');
+        // Check if the settings panel is now visible
+        if (settingsPanel.classList.contains('show-settings')) {
+            // If visible, remove the 'hidden' class to display the panel
+            settingsPanel.classList.remove('hidden');
+        } else {
+            // If hidden, wait for the transition to end, then hide the panel
+            settingsPanel.addEventListener('transitionend', function () {
+                settingsPanel.classList.add('hidden');
+            }, { once: true });
+        }
     });
 });
