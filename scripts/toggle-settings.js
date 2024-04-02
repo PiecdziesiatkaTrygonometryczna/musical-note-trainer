7313773763661b7f07295beec3d7ab55b3708620
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-02 15:25:30 +0200

    beta dispalying settings

--- scripts/toggle-settings.js
+++ scripts/toggle-settings.js
@@ -1,23 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const settingsPanel = document.querySelector('.settings');
     const toggleSettingsButton = document.getElementById('toggleSettings');
 
-    // hide the settings panel by default
-    settingsPanel.style.display = 'none';
-
-    // toggle settings panel visibility when the button is clicked
-    toggleSettingsButton.addEventListener('click', function() {
-        if (settingsPanel.style.display === 'none') {
-            settingsPanel.style.display = 'flex';
-        } else {
-            settingsPanel.style.display = 'none';
-        }
+    toggleSettingsButton.addEventListener('click', function () {
+        settingsPanel.classList.toggle('show-settings');
     });
-
-    // hide the settings panel if user clicks outside of it
-    // document.addEventListener('click', function(event) {
-    //     if (!settingsPanel.contains(event.target) && event.target !== toggleSettingsButton) {
-    //         settingsPanel.style.display = 'none';
-    //     }
-    // });
 });
