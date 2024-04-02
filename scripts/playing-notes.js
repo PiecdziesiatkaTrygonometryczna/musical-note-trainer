fc93917d521fcba9067b70d53d8fc8d8ed9a2393
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-02 22:37:56 +0200

    displaying

--- scripts/playing-notes.js
+++ scripts/playing-notes.js
@@ -1,16 +1,15 @@
-let pause = false;
-
+let pauseSimpleChecker = false;
+// check if currently played note matches displayed note (without an octave)
 document.addEventListener('noteChanged', function (event) {
-    if (!pause) {
+    if (!pauseSimpleChecker) {
         const currentNotePlaying = event.detail.currentNotePlaying;
         const currentNoteNav = document.querySelector(`.${CURRENT_NOTE_CLASS}`);
-        console.log(currentNotePlaying);
 
         if (currentNoteNav.textContent.trim() === currentNotePlaying) {
             console.log("dobrze");
             currentNoteNav.style.color = 'green';
             console.log(withoutInterval);
-            if (withoutInterval) { pause = true; setTimeout(() => { updateNotesDisplay(); pause = false; }, 1000); }
+            if (withoutInterval) { pauseSimpleChecker = true; setTimeout(() => { updateNotesDisplay(); pauseSimpleChecker = false; }, 1000); }
             else if (!withInterval) { setTimeout(() => revertColorOfCurrentNote(), 1000); }
         }
     }
