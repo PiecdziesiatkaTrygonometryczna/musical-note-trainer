5e643be9f33d07179f1b287e98ea41addfc85e72
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-03 14:50:57 +0200

    improved selecting advanced notes

--- scripts/advanced-settings.js
+++ scripts/advanced-settings.js
@@ -10,11 +10,10 @@ function closeAdvancedSettingsWindow() {
 // function to populate note buttons for all octaves
 function populateNoteButtons() {
   let noteButtonsContainer = document.getElementById("noteButtonsContainer");
-
   noteButtonsContainer.innerHTML = "";
   const numOctaves = 8;
 
-  for (let octave = 1; octave <= numOctaves; octave++) {   // generate a div for each octave
+  for (let octave = 1; octave <= numOctaves; octave++) {
     let octaveDiv = document.createElement("div");
     octaveDiv.classList.add("octave-container");
 
@@ -26,20 +25,107 @@ function populateNoteButtons() {
     let buttonsDiv = document.createElement("div");
     buttonsDiv.classList.add("note-buttons");
 
-    const notes = ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B'];     // and buttons for each note in the octave  
+    const notes = ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B'];
     notes.forEach(note => {
       let button = document.createElement("button");
       button.textContent = note + octave;
       button.value = note + octave;
-      button.onclick = function() {
+      button.onclick = function () {
         toggleNoteSelection(this.value);
       };
       buttonsDiv.appendChild(button);
     });
+
+    // button to select all notes in spesific octave
+    let selectOctaveButton = document.createElement("button");
+    selectOctaveButton.textContent = "Wszystkie";
+    selectOctaveButton.onclick = function () {
+      selectOctaveNotes(octave);
+    };
+    buttonsDiv.appendChild(selectOctaveButton);
+
+    // button to select whole notes (without sharps) in specific octave
+    let selectWholeNotesButton = document.createElement("button");
+    selectWholeNotesButton.textContent = "Całe";
+    selectWholeNotesButton.onclick = function () {
+      selectWholeNotesInOctave(octave);
+    };
+    buttonsDiv.appendChild(selectWholeNotesButton);
+
+
+    // button to select sharp notes in specific octave
+    let selectSharpNotesButton = document.createElement("button");
+    selectSharpNotesButton.textContent = "Krzyżyki";
+    selectSharpNotesButton.onclick = function () {
+      selectSharpNotesInOctave(octave);
+    };
+    buttonsDiv.appendChild(selectSharpNotesButton);
+
+    // button to unselect all notes in specific octave
+    let unselectAllButton = document.createElement("button");
+    unselectAllButton.textContent = "Wyczyść";
+    unselectAllButton.onclick = function () {
+      unselectOctaveNotes(octave);
+    };
+    buttonsDiv.appendChild(unselectAllButton);
+
+
+
+
+
     octaveDiv.appendChild(buttonsDiv);
     noteButtonsContainer.appendChild(octaveDiv);
   }
 }
+
+
+// and corresponding functions to handle selection within a specific octabe 
+function selectOctaveNotes(octave) {
+  let noteButtons = document.querySelectorAll("#noteButtonsContainer button");
+  noteButtons.forEach(button => {
+    if (button.textContent.endsWith(octave)) {
+      button.classList.add("selected");
+    }
+  });
+}
+
+function unselectOctaveNotes(octave) {
+  let noteButtons = document.querySelectorAll("#noteButtonsContainer button");
+  noteButtons.forEach(button => {
+    if (button.textContent.endsWith(octave)) {
+      button.classList.remove("selected");
+    }
+  });
+}
+
+function selectWholeNotesInOctave(octave) {
+  let noteButtons = document.querySelectorAll("#noteButtonsContainer button");
+  noteButtons.forEach(button => {
+    if (button.textContent.endsWith(octave)) {
+      if (!button.textContent.includes('♯')) {
+        button.classList.add("selected");
+      } else {
+        button.classList.remove("selected");
+      }
+    }
+  });
+}
+
+function selectSharpNotesInOctave(octave) {
+  let noteButtons = document.querySelectorAll("#noteButtonsContainer button");
+  noteButtons.forEach(button => {
+    if (button.textContent.endsWith(octave)) {
+      if (button.textContent.includes('♯')) {
+        button.classList.add("selected");
+      } else {
+        button.classList.remove("selected");
+      }
+    }
+  });
+}
+
+
+
 
 // this function is called every time a note button is pressed
 function toggleNoteSelection(note) {
@@ -53,7 +139,7 @@ populateNoteButtons();
 
 
 
-// Function to select all notes
+// function to select all notes from all octaves
 function advancedSelectAllNotes() {
   let noteButtons = document.querySelectorAll("#noteButtonsContainer button");
   noteButtons.forEach(button => {
