6fa27dde4d77693f8c3bca4ead06ea8474569a80
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-03 11:59:36 +0200

    advanced settings

---
+++ scripts/advanced-settings.js
@@ -0,0 +1,7 @@
+function openAdvancedSettingsWindow() {
+  document.getElementById("advancedSettingsWindow").style.display = "block";
+}
+
+function closeAdvancedSettingsWindow() {
+  document.getElementById("advancedSettingsWindow").style.display = "none";
+}
