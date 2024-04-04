9d47d7b6267e2d0750f6e2ba3c45150655628c3f
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-04 13:17:52 +0200

    displaying notes for all strings on guitar

--- scripts/fretboard.js
+++ scripts/fretboard.js
@@ -1,4 +1,4 @@
-document.getElementById("stringCheckboxToggle").addEventListener("change", function() {
+document.getElementById("stringECheckboxToggle").addEventListener("change", function() {
     var stringImage = document.getElementById("1st-string-wholes");
     if (this.checked) {
         stringImage.style.display = "inline";
@@ -7,8 +7,99 @@ document.getElementById("stringCheckboxToggle").addEventListener("change", funct
     }
 });
 
-document.getElementById("stringCheckboxToggle2").addEventListener("change", function() {
+document.getElementById("stringECheckboxToggle2").addEventListener("change", function() {
     var stringImage = document.getElementById("1st-string-sharps");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+
+document.getElementById("stringACheckboxToggle").addEventListener("change", function() {
+    var stringImage = document.getElementById("2nd-string-wholes");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringACheckboxToggle2").addEventListener("change", function() {
+    var stringImage = document.getElementById("2nd-string-sharps");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringDCheckboxToggle").addEventListener("change", function() {
+    var stringImage = document.getElementById("3rd-string-wholes");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringDCheckboxToggle2").addEventListener("change", function() {
+    var stringImage = document.getElementById("3rd-string-sharps");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringGCheckboxToggle").addEventListener("change", function() {
+    var stringImage = document.getElementById("4th-string-wholes");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringGCheckboxToggle2").addEventListener("change", function() {
+    var stringImage = document.getElementById("4th-string-sharps");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringBCheckboxToggle").addEventListener("change", function() {
+    var stringImage = document.getElementById("5th-string-wholes");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringBCheckboxToggle2").addEventListener("change", function() {
+    var stringImage = document.getElementById("5th-string-sharps");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringHighECheckboxToggle").addEventListener("change", function() {
+    var stringImage = document.getElementById("6th-string-wholes");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringHighECheckboxToggle2").addEventListener("change", function() {
+    var stringImage = document.getElementById("6th-string-sharps");
     if (this.checked) {
         stringImage.style.display = "inline";
     } else {
