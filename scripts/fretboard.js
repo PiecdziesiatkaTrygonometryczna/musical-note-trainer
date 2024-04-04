0b62fcbbc07d6d6c1c8e4f06a77e96918725e66b
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-04 12:19:14 +0200

    better displaying notes on fretboard

--- scripts/fretboard.js
+++ scripts/fretboard.js
@@ -1,5 +1,14 @@
 document.getElementById("stringCheckboxToggle").addEventListener("change", function() {
-    var stringImage = document.getElementById("stringImage");
+    var stringImage = document.getElementById("1st-string-wholes");
+    if (this.checked) {
+        stringImage.style.display = "inline";
+    } else {
+        stringImage.style.display = "none";
+    }
+});
+
+document.getElementById("stringCheckboxToggle2").addEventListener("change", function() {
+    var stringImage = document.getElementById("1st-string-sharps");
     if (this.checked) {
         stringImage.style.display = "inline";
     } else {
