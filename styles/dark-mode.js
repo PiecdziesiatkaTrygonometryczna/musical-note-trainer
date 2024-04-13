ee524cce5afac7e1752b316c2da881310d7c8ae5
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-13 14:03:19 +0200

    dark mode for insturment icons

--- styles/dark-mode.js
+++ styles/dark-mode.js
@@ -16,4 +16,23 @@ modeToggleBtn.addEventListener('click', () => {
         modeIcon.classList.add('fa-sun');
         modeIcon.style.color = '';
     }
+
+    const guitarIcon = document.getElementById('toggleFretboardButton');
+    const keyboardIcon = document.getElementById('togglePianoButton');
+    const bassIcon = document.getElementById('toggleBassButton');
+    if (isDarkMode) {
+        // Select the elements by their IDs
+
+
+        // Change the src attribute of each image to the white version
+        guitarIcon.src = "img/guitar-icon-white.png";
+        keyboardIcon.src = "img/keyboard-icon-white.png";
+        bassIcon.src = "img/bass-icon-white.png";
+    }
+    else {
+        guitarIcon.src = "img/guitar-icon.png";
+        keyboardIcon.src = "img/keyboard-icon.png";
+        bassIcon.src = "img/bass-icon.png";
+    }
+
 });
